6776f9d47ef475daa5879d57a707b3bcab8b9fd2
raneeshchoudhary <raneeshchoudhary@gmail.com>
2024-08-17 19:13:28 +0530

    firefox execution

--- src/main/java/Base/DriverManager.java
+++ src/main/java/Base/DriverManager.java
@@ -2,13 +2,15 @@ package Base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverManager {
 
     private static ThreadLocal<WebDriver> threadLocalDriver= new ThreadLocal<>();;
 
     public static WebDriver initDriver(){
-        threadLocalDriver.set(new ChromeDriver());
+//        threadLocalDriver.set(new ChromeDriver());
+        threadLocalDriver.set(new FirefoxDriver());
         return threadLocalDriver.get();
     }
 
