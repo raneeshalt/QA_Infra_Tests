44e9b1fc7f85b9dda2b3e3e45a6a0f28a475ed5e
raneeshchoudhary <raneeshchoudhary@gmail.com>
2024-08-17 19:28:04 +0530

    chrome driver

--- src/main/java/Base/DriverManager.java
+++ src/main/java/Base/DriverManager.java
@@ -2,15 +2,27 @@ package Base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverManager {
 
     private static ThreadLocal<WebDriver> threadLocalDriver= new ThreadLocal<>();;
 
     public static WebDriver initDriver(){
-//        threadLocalDriver.set(new ChromeDriver());
-        threadLocalDriver.set(new FirefoxDriver());
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+        chromeOptions.addArguments("--headless");
+
+        threadLocalDriver.set(new ChromeDriver(chromeOptions));
+//        threadLocalDriver.set(new FirefoxDriver(options));
         return threadLocalDriver.get();
     }
 
